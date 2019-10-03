b08127400a87152592e71076748032f12852abbf
Disnaka <52813193+Disnaka@users.noreply.github.com>
2019-10-03 11:36:21 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,13 @@
+<div class="header">
+    	<div class="logo">
+        <a href="index.php"><img src="images/logo.jpg" alt="SMGO" /></a>
+        </div><!--logo ends-->
+		<div class="navigation">
+		<p><?php 
+			include("categoryList.php");
+		?>
+        <a href="#">Blogs</a>
+        <a href="index.php?page=admin">Vendor</a>
+        </p>
+      </div><!--navigation ends-->
+	</div><!-- Header ends here-->
